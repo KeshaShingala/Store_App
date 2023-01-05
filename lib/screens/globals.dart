d2cb6d831712278c8042ffe0fe9a293cbeab1f28
KeshaShingala <keshashingala>
2023-01-05 23:58:59 +0530

    Product_app

---
+++ lib/screens/globals.dart
@@ -0,0 +1,26 @@
+
+class Details {
+  String name = "";
+  String image = "";
+  int price = 0;
+
+  Details(
+      {required this.name, required this.price, required this.image});}
+
+List<Details> productList = [
+  Details(name: "Vagabond sack", price: 120, image: "assets/images/sack.jfif"),
+ Details(name: "Stella Sunglasses", price: 58, image: "assets/images/vincent.jpg"),
+  Details(name: "Whitney belt", price: 35, image: "assets/images/fab.jpg"),
+  Details(name: "Garden strand", price: 98, image: "assets/images/garden.jpg"),
+  Details(name: "Strut Earings", price: 34, image: "assets/images/earrind.jfif"),
+  Details(name: "Weave Keyring", price: 16, image: "assets/images/weave.jpg"),
+Details(name: "Cloth", price: 45, image: "assets/images/cloth.jfif"),
+  Details(name: "golden necklace", price: 160, image: "assets/images/goladen nechlace.jpg"),
+  Details(name: "Handmade", price: 56, image: "assets/images/handmade-painting.jpg"),
+  Details(name: "Sofa", price: 100, image: "assets/images/antalya-3-seater-sofa.png"),
+  Details(name: "Tables", price:99, image: "assets/images/Table.jfif"),
+];
+
+List<Details> cart = [
+  Details(name: "Weave Keyring", price: 16, image: "assets/images/weave.jpg"),
+];
